b0c3aece4f289912ffcf31a34aa5f28e14b2d49c
Nathan Wallace <nathan@nosuchthingastwo.com>
2016-03-01 19:25:56 -0500

    upgrade midje version

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject langton "0.0.1-SNAPSHOT"
   :description "Cool new project to do things and stuff"
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/tools.cli "0.3.3"]
                  [clojure-term-colors "0.1.0-SNAPSHOT"]]
-  :profiles {:dev {:dependencies [[midje "1.5.1"]]
+  :profiles {:dev {:dependencies [[midje "1.8.3"]]
                    :plugins [[lein-midje "3.1.3"]]}}
   :main langton.core)
